825954d4b91f87c0b9cb811a143bc651967b5377
doohyun1998 <ehgusrlaeh@naeileun.dev>
2024-04-16 09:45:57 +0900

    setting prettier & eslint

--- __test__/app.spec.ts
+++ __test__/app.spec.ts
@@ -1,7 +1,7 @@
 const welcomeTest = () => {
-  test("./welcome", () => {
-    const result = "welcome";
-    expect(result).toEqual("welcome"); // result 값이 "welcome"와 같은지 확인
+  test('./welcome', () => {
+    const result = 'welcome';
+    expect(result).toEqual('welcome'); // result 값이 "welcome"와 같은지 확인
   });
 };
-describe("welcome test", welcomeTest); // describe 함수에 테스트 함수 welcomeTest를 전달하여 실행
+describe('welcome test', welcomeTest); // describe 함수에 테스트 함수 welcomeTest를 전달하여 실행
